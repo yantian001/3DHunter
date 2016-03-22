08fb0c4b099bb32c74a88df2c8b4e3a2b2ac1dcd
yantian001@vip.qq.com <yantian001@vip.qq.com>
2016-03-22 15:11:09 +0800

    commit by hugo

---
+++ Assets/rotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        LeanTween.rotate(gameObject,transform.localRotation.eulerAngles + new Vector3(0, 359, 0), 0.5f).setRepeat(1111111);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
